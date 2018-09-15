f594533e9198e61552051815f311694f21b54fe9
Iavor Diatchki <iavor.diatchki@gmail.com>
2018-09-15 16:32:38 +0300

    Checkpoint:
    1. Break up cases, to be more managable
    2. Structured data can be nested (e.g. [ [1,2], [3,4] ]), so when lifting
       things we have to recursively lift all the way to the leaves.

--- Language/Lustre/Transform/NoStruct.hs
+++ Language/Lustre/Transform/NoStruct.hs
@@ -19,6 +19,7 @@ data Env = Env
     -- ^ Contains the expansions for variables of strucutred types.
     -- For example, if @x : T ^ 3@, then we shoud have a binding
     -- @x = [ a, b, c ]@.
+    -- The expressions in the map should be in evaluated form.
 
   , nodeTypes :: Map Name NodeProfile
     -- ^ Information about the types of the nodes that are in scope.
@@ -41,6 +42,133 @@ caseThruRange expr f =
     ERange r e -> ERange r (caseThruRange e f)
     _          -> f expr
 
+
+{- | Move @when@ to the leaves of a structured expressions.
+The parameters should be already evaluated.
+
+@[a,b] when c   -->    [a when c, b when c ]@
+
+Note that clock expressions (e.g., `c` above) are small,
+so it is OK to duplicate them. -}
+
+evalWhen :: Expression -> ClockExpr -> Expression
+evalWhen e ce =
+  caseThruRange e $ \ev ->
+    case ev of
+      Tuple xs -> Tuple [ x `evalWhen` ce | x <- xs ]
+      Array xs -> Array [ x `evalWhen` ce | x <- xs ]
+
+      Struct s mb fs ->
+        case mb of
+          Nothing -> Struct s Nothing
+                       [ Field l (f `evalWhen` ce) | Field l f <- fs ]
+
+          Just _  -> panic "evalWhen" [ "Unexpected struct update"
+                                      , "*** Expression: " ++ showPP e
+                                      ]
+      e1' -> e1' `When` ce
+
+
+{- | Move a @merege@ to the leaves of structured data.
+The branches of the case should have been evaluated already.
+
+@ merge c (A -> [1,2]; B -> [3,4])  -->
+becomes
+[ merge c (A -> 1; B -> 3), merge c (A -> 2; B -> 4) ]
+@
+
+Again here we assume that patterns are simple things, as they should be
+-}
+
+evalMerge :: Ident -> [MergeCase] -> Expression
+evalMerge i as =
+  case as of
+    [] -> panic "evalMerge" [ "Empty merge case" ]
+    opts@(MergeCase _ o : _) ->
+      case getShape o of
+        Nothing -> Merge i opts
+        Just sh -> rebuildShape sh mk [ e | MergeCase _ e <- opts ]
+          where
+          mk es' = evalMerge i
+                     [ MergeCase p e | (MergeCase p _, e) <- zip opts es' ]
+
+
+
+-- | Evaluate a struct update
+evalStructUpdate :: Env -> Name {- type -} -> Name -> [Field] -> Expression
+evalStructUpdate env s x es =
+  case lkpStrName x env of
+    Just e ->
+      caseThruRange e $ \ev ->
+        case ev of
+          Struct s' Nothing fs | s == s' ->
+            Struct s Nothing
+              [ Field l (Map.findWithDefault v l fldMap) | Field l v <- fs ]
+
+
+          _ -> panic "evalExpr" [ "Unexpected value to update:"
+                                , "*** Expected: a struct"
+                                , "*** Expression: " ++ showPP e
+                                ]
+    Nothing -> panic "evalExpr"
+                    [ "Missing structure expression for:"
+                    , "*** Name: " ++ showPP x
+                    ]
+
+  where
+  fldMap = Map.fromList [ (l, evalExpr env v) | Field l v <- es ]
+
+selectFromArray :: [Expression] -> Selector Integer -> Expression
+selectFromArray vs s =
+  case s of
+
+    SelectField f ->
+      panic "evalExpr"
+        [ "Attempt to select a field from an array."
+        , "*** Field: " ++ showPP f
+        , "*** ArrayL " ++ showPP (Array vs)
+        ]
+
+    SelectElement i -> getIx i
+
+    SelectSlice sl ->
+      let step  = fromMaybe 1 (arrayStep sl)
+          start = arrayStart sl
+          ixes  = [ start, start + step .. arrayEnd sl ]
+      in Array (map getIx ixes)
+
+  where
+  getIx i = case genericDrop i vs of
+              v : _ -> v
+              _ -> panic "selectFromArray"
+                     [ "Selector out of bounds:"
+                     , "*** Index: " ++ show i
+                     , "*** Array: " ++ showPP (Array vs)
+                     ]
+
+selectFromStruct :: Name -> [Field] -> Selector Integer -> Expression
+selectFromStruct ty fs s =
+    case s of
+
+      SelectField i ->
+        case [ v | Field l v <- fs, l == i ] of
+          v : _ -> v
+          _ -> panic "selectFromStruct"
+                 [ "Undefined field in selection:"
+                 , "*** Field: " ++ showPP i
+                 , "*** Struct: " ++ showPP (Struct ty Nothing fs)
+                 ]
+
+      _ -> panic "selectFromStruct"
+             [ "Type error in selector."
+             , "*** Selector: " ++ showPP s
+             , "*** Struct: " ++ showPP (Struct ty Nothing fs)
+             ]
+
+
+
+
+
 -- | Normalize an expression, lifting out structured data to the top.
 evalExpr :: Env -> Expression -> Expression
 evalExpr env expr =
@@ -54,29 +182,9 @@ evalExpr env expr =
 
     Lit _ -> expr
 
-    {- [a,b] when c   -->    [a when c, b when c ]
-
-    Note that clock expressions (e.g., `c` above) are small,
-    so it is OK to duplicate them.  We also assume that they don't contain
-    structured data, which is why we don't evaluate them---
-    they are supposed to be simple expressions (e.g., no selectors)
-    of boolean or enum type. -}
-    e1 `When` ce ->
-
-      caseThruRange (evalExpr env e1) $ \ev ->
-        case ev of
-          Tuple xs -> Tuple [ x `When` ce | x <- xs ]
-          Array xs -> Array [ x `When` ce | x <- xs ]
-
-          Struct s mb fs ->
-            case mb of
-              Nothing -> Struct s Nothing
-                           [ Field l (e `When` ce) | Field l e <- fs ]
-
-              Just _  -> panic "evalExpr" [ "Unexpected update"
-                                          , "*** Expression: " ++ showPP expr
-                                          ]
-          e1' -> e1' `When` ce
+    -- The clock expression are syntactically restricted to not
+    -- contain structured data so we don't need to evaluate them.
+    e1 `When` ce -> evalWhen (evalExpr env e1) ce
 
     Tuple es -> Tuple (map (evalExpr env) es)
 
@@ -86,85 +194,19 @@ evalExpr env expr =
       case mb of
         Nothing -> Struct s Nothing
                       [ Field l (evalExpr env e) | Field l e <- es ]
-        Just x ->
-          case lkpStrName x env of
-            Just e ->
-              caseThruRange e $ \ev ->
-                case ev of
-                  Struct s' Nothing fs | s == s' ->
-                    Struct s Nothing
-                      [ Field l (Map.findWithDefault v l exprMap)
-                                                      | Field l v <- fs ]
-                    where
-                    exprMap = Map.fromList
-                               [ (l, evalExpr env v) | Field l v <- es ]
 
-
-                  _ -> panic "evalExpr" [ "Unexpected value to update:"
-                                        , "*** Expected: a struct"
-                                        , "*** Expression: " ++ showPP e
-                                        ]
-
-            Nothing -> panic "evalExpr"
-                            [ "Missing structure expression for:"
-                            , "*** Name: " ++ showPP x
-                            ]
+        Just x -> evalStructUpdate env s x es
 
     Select e sel ->
 
       caseThruRange (evalExpr env e) $ \ev ->
         case ev of
-
-          Array vs ->
-            case s of
-
-              SelectField f ->
-                panic "evalExpr"
-                  [ "Attempt to select a field from an array."
-                  , "*** Field: " ++ showPP f
-                  , "*** ArrayL " ++ showPP (Array vs)
-                  ]
-
-              SelectElement i -> getIx i
-
-              SelectSlice sl ->
-                let step  = fromMaybe 1 (arrayStep sl)
-                    start = arrayStart sl
-                    ixes  = [ start, start + step .. arrayEnd sl ]
-                in Array (map getIx ixes)
-
-            where getIx i =
-                    case genericDrop i vs of
-                      v : _ -> v
-                      _ -> panic "evalExpr"
-                             [ "Selector out of bounds:"
-                             , "*** Index: " ++ show i
-                             , "*** Array: " ++ showPP (Array vs)
-                             ]
-
-          Struct _ Nothing fs ->
-            case s of
-
-              SelectField i ->
-                case [ v | Field l v <- fs, l == i ] of
-                  v : _ -> v
-                  _ -> panic "evalExpr"
-                         [ "Undefined field in selection:"
-                         , "*** Field: " ++ showPP i
-                         , "*** Struct: " ++ showPP ev
-                         ]
-
-              _ -> panic "evalExpr"
-                     [ "Type error in selector."
-                     , "*** Selector: " ++ showPP s
-                     , "*** Struct: " ++ showPP ev
-                     ]
-
-          _ -> panic "evalExpr"
-                  [ "Selection from a non structured type:"
-                  , "*** Expression: " ++ showPP ev
-                  ]
-
+          Array vs            -> selectFromArray vs s
+          Struct ty Nothing fs -> selectFromStruct ty fs s
+          _                   -> panic "selectFromStruct"
+                                    [ "Selection from a non structured type:"
+                                    , "*** Expression: " ++ showPP ev
+                                    ]
       where s = evalSelect sel
 
 
@@ -173,21 +215,10 @@ evalExpr env expr =
                         , "*** Should have been eliminated by 'NoStatic'"
                         ]
 
-    -- merge c (A -> [1,2]; B -> [3,4])  -->
-    -- becomes
-    -- [ merge c (A -> 1; B -> 3), merge c (A -> 2; B -> 4) ]
-    --
-    -- Again here we assume that patterns are simple things, as they should be
     Merge i as ->
-      case [ MergeCase p (evalExpr env e) | MergeCase p e <- as ] of
-        [] -> panic "evalExpr" [ "Empty merge case" ]
-        opts@(MergeCase _ o : _) ->
-          case getShape o of
-            Nothing -> Merge i opts
-            Just sh -> rebuildShape sh mk [ e | MergeCase _ e <- opts ]
-              where mk es' = Merge i [ MergeCase p e |
-                                          (MergeCase p _, e) <- zip opts es' ]
+      evalMerge i [ MergeCase p (evalExpr env e) | MergeCase p e <- as ]
 
+    -- XXX: DEAL WITH NESTED DATA + ITERATORS
     CallPos f es ->
       case (f, map (evalExpr env) es) of
 
